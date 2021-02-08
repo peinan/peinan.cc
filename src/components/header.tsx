6615a20ce9cda4d39fb75b16433518be0b92040f
Peinan ZHANG <zhang_peinan@cyberagent.co.jp>
2021-02-08 12:51:58 +0900

    use line-awesome from node_modules

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -22,32 +22,6 @@ export default ({ titlePre = '' }) => {
       <Head>
         <title>{titlePre ? `${titlePre} |` : ''} Peinan's Chronicle</title>
 
-        {/*<link rel="preconnect" href="https://fonts.gstatic.com"></link>*/}
-        {/*<link*/}
-        {/*  rel="stylesheet"*/}
-        {/*  href="https://fonts.googleapis.com/css?family=Lobster"*/}
-        {/*></link>*/}
-        {/*<link*/}
-        {/*  rel="stylesheet"*/}
-        {/*  href="https://fonts.googleapis.com/css?family=Niconne"*/}
-        {/*></link>*/}
-        {/*<link*/}
-        {/*  rel="stylesheet"*/}
-        {/*  href="https://fonts.googleapis.com/css?family=Poppins:wght@300,500,700&display=swap"*/}
-        {/*></link>*/}
-        {/*<link*/}
-        {/*  rel="stylesheet"*/}
-        {/*  href="https://fonts.googleapis.com/css?family=Inter:wght@300,500,700&display=swap"*/}
-        {/*></link>*/}
-        {/*<link*/}
-        {/*  rel="stylesheet"*/}
-        {/*  href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:wght@300,500,700&display=swap"*/}
-        {/*></link>*/}
-        <link
-          rel="stylesheet"
-          href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css"
-        />
-
         <meta name="title" content="Peinan's Chronicle" />
         <meta
           name="description"
