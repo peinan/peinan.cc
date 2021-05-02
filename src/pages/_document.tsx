dc6729287c47c294f1304b63cd617735138951d8
Peinan ZHANG <zhang_peinan@cyberagent.co.jp>
2021-05-02 23:26:42 +0900

    added Google Analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,33 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+
+import { existsGaId, GA_TRACKING_ID } from '../lib/gtag'
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {existsGaId && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GA_TRACKING_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+                }}
+              />
+            </>
+          )}
+        </Head>
         <body>
           <script> </script>
           <Main />
