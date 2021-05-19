fd176bc92ac331f1be905aeb4607759d678e24b0
Peinan ZHANG <zhang_peinan@cyberagent.co.jp>
2021-05-20 00:09:37 +0900

    added sitemaps (#70)

---
+++ next-sitemap.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteUrl: 'https://www.peinan.cc',
+  generateRobotsTxt: true,
+  exclude: ['/server-sitemap.xml'],
+  robotsTxtOptions: {
+    additionalSitemaps: ['https://www.peinan.cc/server-sitemap.xml'],
+  },
+}
