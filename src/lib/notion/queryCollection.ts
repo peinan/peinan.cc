15d22f801b82e00e32530ad692ca4a2daf1fc78f
Peinan ZHANG <zhang_peinan@cyberagent.co.jp>
2022-01-16 20:59:38 +0900

    updated queryCollection body

--- src/lib/notion/queryCollection.ts
+++ src/lib/notion/queryCollection.ts
@@ -6,50 +6,31 @@ export default function queryCollection({
   loader = {},
   query = {},
 }: any) {
-  const {
-    limit = 999, // TODO: figure out Notion's way of handling pagination
-    loadContentCover = true,
-    type = 'table',
-    userLocale = 'en',
-    userTimeZone = 'America/Phoenix',
-  } = loader
-
-  const {
-    aggregate = [
-      {
-        aggregation_type: 'count',
-        id: 'count',
-        property: 'title',
-        type: 'title',
-        view_type: 'table',
+  const queryCollectionBody = {
+    loader: {
+      type: 'reducer',
+      reducers: {
+        collection_group_results: {
+          type: 'results',
+          limit: 999,
+          loadContentCover: true,
+        },
+        'table:uncategorized:title:count': {
+          type: 'aggregation',
+          aggregation: {
+            property: 'title',
+            aggregator: 'count',
+          },
+        },
       },
-    ],
-    filter = [],
-    filter_operator = 'and',
-    sort = [
-      // TDOO: workaround to get posts sorted by "Created At" property
-      {
-        property: 'piCV',
-        direction: 'descending',
-      },
-    ],
-  } = query
+      searchQuery: '',
+      userTimeZone: 'America/Phoenix',
+    },
+  }
 
   return rpc('queryCollection', {
     collectionId,
     collectionViewId,
-    loader: {
-      limit,
-      loadContentCover,
-      type,
-      userLocale,
-      userTimeZone,
-    },
-    query: {
-      aggregate,
-      filter,
-      filter_operator,
-      sort,
-    },
+    ...queryCollectionBody,
   })
 }
